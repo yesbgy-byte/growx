1c450ea8f421e4811592ade6fc1a6aad31e4c562
yesbgy-byte <yesbgy@gmail.com>
2025-11-27 18:08:32 +0900

    feat: add vercel analytics

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Analytics } from '@vercel/analytics/react';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -11,5 +13,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
